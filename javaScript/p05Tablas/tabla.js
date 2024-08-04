74478780b0771d60bb2ce62672fd8738ce59056f
Lotfi <Latifiyates@gmail.com>
2024-08-04 17:48:15 +0200

    Añadimos los inputs

--- javaScript/p05Tablas/tabla.js
+++ javaScript/p05Tablas/tabla.js
@@ -3,7 +3,7 @@ const body = document.body;
 const menuBody = document.getElementById("menuBody");
 const menuTabla = document.getElementById("menutabla");
 const tablasElemento =  document.getElementById("tablas");
-var tablaSelect = {tablaid : null , filaid : null , celda : null};
+var tablaSelect = {tablaid : document.getElementById("") , filaid : document.getElementById("") , celda : document.getElementById("")};
 const tablas = [];
 
 // eventos
@@ -20,6 +20,9 @@ document.body.addEventListener("mousedown", function(e) {
     menuTabla.style.display="none";
 });
 
+
+
+
 document.addEventListener("contextmenu", function(e) {
     e.preventDefault();
 });
@@ -27,51 +30,66 @@ document.addEventListener("contextmenu", function(e) {
 ////menubody
 document.getElementById("menuBody").addEventListener("mousedown",function(e){
     e.stopPropagation();
-})
-
-document.getElementById("CrearTabla").addEventListener("click",function(e){
-    creadorTabla()
     if(e.button == 0) menuBody.style.display = "none";
 })
-document.getElementById("CargarTabla").addEventListener("mousedown",function(e){
-    console.log("hola")
-    if(e.button == 0) creadorTabla("hola",null,CodificarTabla(tablaSelect.tablaid))
-    })
+
+document.getElementById("CrearTabla").addEventListener("mousedown",function(e){
+    if(e.button == 0) creadorTablaVacia()
+})
+
 
 
 ////menuTabla
-document.getElementById("EliminarTabla").addEventListener("mousedown",function(e){
-    if(e.button == 0) eliminarTabla()
-    })
-document.getElementById("EliminarFila").addEventListener("mousedown",function(e){
-    if(e.button == 0) eliminarFila()
-    })
-document.getElementById("EliminarCelda").addEventListener("mousedown",function(e){
-    if(e.button == 0) eliminarCelda()
-    })
-
+/////////CrearElementos
 document.getElementById("AñadirFila").addEventListener("mousedown",function(e){
-    if(e.button == 0) añadirFila()
-    })
-document.getElementById("AñadirCelda").addEventListener("mousedown",function(e){
-    if(e.button == 0) añadirCelda()
-    })
-document.getElementById("CodificarTabla").addEventListener("mousedown",function(e){
-    console.log(CodificarTabla(tablaSelect.tablaid).toString() )
+    if(e.button == 0) creadorFilasVacias(0)
+})
+
+document.getElementById("AñadirAriba").addEventListener("mousedown",function(e){
+    if(e.button == 0) creadorFilasVacias(-1)
+})
+
+document.getElementById("AñadirAbajo").addEventListener("mousedown",function(e){
+    if(e.button == 0) creadorFilasVacias(1)
 })
 
 
+
+////////EliminarElementos
+document.getElementById("EliminarTabla").addEventListener("mousedown",function(e){
+    if(e.button == 0) tablaSelect.tablaid.remove()
+})
+
+document.getElementById("EliminarTabla").addEventListener("mousedown",function(e){
+    if(e.button == 0) tablaSelect.tablaid.remove()
+})
+
+document.getElementById("EliminarFila").addEventListener("mousedown",function(e){
+    if(e.button == 0) tablaSelect.filaid.remove()
+})
 ////eventosFunciones
 
 document.getElementById("tablas").addEventListener("mousedown", function(e) {
     e.stopPropagation();
+    selecionarTabla();
     if(e.button==2){
-        selecionarTabla();
         console.log(tablaSelect.tablaid)
         menuTabla.style.display = 'flex'
         menuTabla.style.left  = e.clientX  
         menuTabla.style.top =  e.clientY
+        if(!tablaSelect.filaid.classList.contains("filas")){
+            for (const elemento of document.getElementsByClassName("menutabla1")) {
+                elemento.style.display = "none";
+            }
+        }else{
+            for (const elemento of document.getElementsByClassName("menutabla1")) {
+                elemento.style.display = "";
+            }
+        }
     }else{
+        const celda = tablaSelect.celda
+        //Marcar fila      tablaSelect.filaid.style.border = "red 3px solid"
+        celda.style.border = "red 3px solid"
         menuTabla.style.display = 'none'
     }
     menuBody.style.display="none"
@@ -80,113 +98,61 @@ document.getElementById("tablas").addEventListener("mousedown", function(e) {
 
 
 
-/*
-function cargarFuncionesTablas(){
-    document.querySelectorAll(".tablas").forEach( (tabla) => {
-        tabla.addEventListener("mousedown", function(e) {
-            e.stopPropagation();
-            if(e.button==2){
-                selecionarTabla();
-                console.log(tablaSelect.tablaid)
-                menuTabla.style.display = 'flex'
-                menuTabla.style.left  = e.clientX  
-                menuTabla.style.top =  e.clientY
-            }
-            menuBody.style.display="none"
-            
-        });
-        
-    })
-}
-*/
+
+
 
 
 //Funciones
+
 function selecionarTabla(){
     tablaSelect.tablaid = document.querySelector(".tablas:hover")
     tablaSelect.filaid = tablaSelect.tablaid.querySelector("tr:hover")
-    tablaSelect.celda = tablaSelect.filaid.querySelector("td:hover")
+    tablaSelect.celda = tablaSelect.filaid.querySelector("td:hover") 
 }
-function creadorTabla(Titulo,num,filas){
-    num = num || 4;
-    Titulo = Titulo || "Sin Titulo"
+
+function creadorTablaVacia(){
     const tabla = document.getElementById("tablas").appendChild(document.createElement("table"))
     tabla.className = "tablas"
-    const th = tabla.appendChild(document.createElement("tr")).appendChild(document.createElement("th"))
-    th.innerText = Titulo;
-    th.setAttribute("colspan",1000)
-    creadorFilas(tabla,filas,num)
-    cargarFuncionesTablas();
+    tablaSelect.tablaid = tabla;
+    const th = document.createElement("th")
+    th.setAttribute("colspan",10000)
+    const input = document.createElement("input")
+    input.className = "txt"
+    input.value = "SIN TITULO"
+    th.appendChild(input)
+    tabla.appendChild(document.createElement("tr")).appendChild(th)
+    creadorFilasVacias()
 }
-
-function creadorFilas(tabla,filas,num){
-    
-    for(let i = 0; i< (filas ? filas.length : 1 );i++){
-        const tr =  tabla.appendChild(document.createElement("tr"))
-        var fila = (filas ?  filas[i].length==0 ? null : filas[i]  : null)  ;
-        for(let j = 0; j< ( fila ? fila.length : num? num : getMaxColumn(tabla));j++){
-            const td = tr.appendChild(document.createElement("td"));
-            td.setAttribute("colspan",fila ? fila[j][0] : 1);
-            td.setAttribute("rowspan",fila ? fila[j][1] : 1);
-        }
-        
+function creadorFilasVacias(posicion = 0 ){
+    const tabla = tablaSelect.tablaid
+    const num = getMaxColumn(tabla);
+    const tr = document.createElement("tr")
+    tr.className = "filas"
+    for(let i = 0; i < num; i++){
+        const td = document.createElement("td")
+        td.className = "celda"
+        const input = document.createElement("input")
+        input.className = "txt"
+        td.appendChild(input)
+        tr.appendChild(td)
     }
-    
-    /*
-    var txt = ""
-    for(let i = 0; i< (filas ? filas.length : 1 );i++){
-    var fila = (filas ?  filas[i].length==0 ? null : filas[i]  : null)  ;
-    txt += "<tr>";
-    for(let j = 0; j< ( fila ? fila.length : num? num : getMaxColumn(tablaId));j++){
-    txt += `<td colspan="${fila ? fila[j][0] : 1}"  rowspan="${fila ? fila[j][1] : 1}"  ></td>`;
+    switch (posicion) {
+        case 0:
+            tabla.appendChild(tr);
+            break;
+        case 1:
+            tablaSelect.filaid.after(tr);
+            break;
+        case -1:
+            tablaSelect.filaid.before(tr);
+            break;
     }
-    txt += "</tr>";
-    
-    }
-    
-    tabla.innerHTML += txt;
-    */
-    
+    //Intento
+    // for (var td of tabla.getElementsByTagName("td")){
+    //     td.style.width = `${(1/getMaxColumn(tabla))*100}%`;
+    // }
 }
 
-function eliminarTabla(){
-    tablaSelect.tablaid.remove();
-}
-
-function eliminarFila(){
-    tablaSelect.filaid.remove();
-}
-
-function eliminarCelda(){
-    tablaSelect.celda.remove();
-}
-
-function añadirFila(){
-    creadorFilas(tablaSelect.tablaid,[[]])
-}
-
-function añadirCelda(){
-    tablaSelect.filaid.appendChild(document.createElement("td"))
-}
-
-function getMaxColumn(tabla){
-    const columns = 0;
-    //const pr = document.querySelector(`.tablas[tablaid=${tablaId}]`).getElementsByTagName("tr") 
-    //for(const num of pr )  if(num.childElementCount > columns){columns = num.childElementCount} 
-    return  4;
-}
-
-function CodificarTabla(tablaId){
-    const tabla = document.querySelector(`.tablas[tablaid="${tablaId}"]`).getElementsByTagName("tr")
-    const cod1 = [];
-    for(let i =1;i<tabla.length;i++){
-        console.log("Añadiendo FIla");
-        const cod2 = [];
-        for(let j =0;j<tabla[i].getElementsByTagName("td").length;j++){
-            console.log("Columna");
-            cod2.push([]);
-        }
-        cod1.push(cod2);
-    }
-    return cod1;
+function getMaxColumn(tabla = document.getElementById("")){
+    return tabla.children[1]? tabla.children[1].childElementCount : 6;
 }
