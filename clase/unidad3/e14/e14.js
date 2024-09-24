928c2cbd424f93806b9e1f3db73d57de5904d15e
Latifi <latifiyates@gmial.com>
2024-09-24 21:31:34 +0200

    unidad3TareaFinalizadaJS

---
+++ clase/unidad3/e14/e14.js
@@ -0,0 +1,31 @@
+print = alert
+let txt = "";
+let patron = /(DOC|025P)([01][0-9][0-9]|2([0-4][0-9]|5[012]))[ABC]/
+let input = document.getElementById("input")
+let b = document.getElementById("button")
+b.addEventListener("click",()=>{
+    txt = input.value;
+    if(patron.test(txt)){
+        print(getIp()+txt.substring(3,6))
+    }else{
+        print("El texto no sigue los criterios")
+    }
+})
+
+
+
+function getIp() {
+    switch (txt.charAt(txt.length - 1)) {
+        case "A":
+            return "10.42.68."
+            break;
+        case "B":
+            return "10.42.69."
+            break;
+        case "C":
+            return "10.52.178."
+            break;
+    }
+}
+
+
