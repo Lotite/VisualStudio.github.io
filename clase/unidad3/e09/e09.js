16d74757b6192e8580ac42fba2fbc549ba5b7120
Latifi <latifiyates@gmial.com>
2024-09-24 18:46:07 +0200

    correcion9js

--- clase/unidad3/e09/e09.js
+++ clase/unidad3/e09/e09.js
@@ -5,8 +5,10 @@ let b = document.getElementById("button")
 b.addEventListener("click", () => ejecutar())
 digitos.addEventListener("keydown",(e)=>{
     if(digitos.value.length>=8){
-        char.value = /[A-Za-z]/.test(e.key) ? e.key : "";
+        if(/[A-Za-z]{1}/.test(e.key)  && e.key.length == 1 ){
+        char.value = e.key
         char.focus()
+        }
     }
     if(!(/[0-9]/.test(e.key) || e.key=="Backspace" )){
         e.preventDefault()
@@ -17,6 +19,7 @@ char.addEventListener("keydown",(e)=>{
     if(!(/[A-Za-z]/.test(e.key) || e.key=="Backspace" )){
         e.preventDefault()
     }
+    if(e.key=="Enter") ejecutar()
 })
 
 
