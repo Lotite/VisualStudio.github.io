fedd020c95246c00428015b0585a3bc635ab1ea5
latifi <latifiyates@gmail.com>
2024-10-08 11:06:38 +0200

    correcion

--- clase/js/unidad3/e02/e02.js
+++ clase/js/unidad3/e02/e02.js
@@ -1,6 +1,8 @@
 let dia = prompt("Ingrese Dia de cumpleaños")
 let mes = prompt("ingrese mes de cumpleaños")
-let fecha = new Date("2024-11-28")
+let fecha = new Date()
+fecha.setDate(dia)
+fecha.setMonth(mes)
 while(fecha.getFullYear()<2100 ) {
     if(fecha.getDay()==0) document.body.innerHTML+=fecha.getFullYear() + "<br/>"
     fecha.setFullYear(fecha.getFullYear()+1);}
