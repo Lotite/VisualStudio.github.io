498bb3f03b866700bcb9378f04158513644e2aa2
Latifi <Latifiyates@gmail.com>
2024-10-25 23:48:27 +0200

    unidad5e6FuncionalidadBasica

--- clase/js/unidad5/e06Cartas/carta.js
+++ clase/js/unidad5/e06Cartas/carta.js
@@ -1,4 +1,109 @@
-import { rotar } from "./estilo.js";
-document.querySelector(".carta").addEventListener("click",()=>{
-    rotar(document.querySelector(".carta"),"pokemon.png")
+let seleccionado = null
+class Carta {
+    #num;
+    #carta;
+    #funRotar;
+    #selecTemporal = null;
+    #lado = "espalda"
+    constructor(num) {
+        this.#num = num
+        this.#carta = this.#crearCarta();
+        this.#funRotar = this.#rotar.bind(this);
+    }
+
+
+    #crearCarta() {
+        let tr = document.createElement("td")
+        tr.innerHTML = `<div  class="carta"></div>`
+        return tr;
+    }
+
+    imprimir() {
+        this.#añadirEvento(this.#carta)
+        return this.#carta;
+    }
+
+
+    #añadirEvento() {
+        this.#carta.addEventListener("click", this.#funRotar)
+    }
+    #eliminarEvento() {
+        this.#carta.removeEventListener("click", this.#funRotar)
+    }
+
+
+   
+
+    #rotar() {
+        if (this.#lado == "espalda") {
+            this.#rotacion(`imagenes/c${this.#num}.png`, 0, 180, 180);
+           this.#lado = "frontal";
+            this.#aciones();
+        } else {
+            this.#lado = "espalda";
+            this.#rotacion("carta.png", 180, 360, 0);
+            if(seleccionado && seleccionado.#carta==this.#carta){
+                seleccionado = false;
+            }
+        }
+    }
+
+
+    #rotacion(imagen, inicio, medio, final) {
+        let carta = this.#carta.firstChild;
+        carta.style.transform = `perspective(700px) rotateY(${inicio + 90}deg)`
+        setTimeout(() => {
+            carta.style.transition = "all 300ms linear,background-image 0ms ease-in-out"
+            carta.style.backgroundImage = `url(${imagen})`
+            carta.style.transform = `perspective(700px) rotateY(${medio}deg)`
+            carta.style.transform = `rotateY(${final}deg)`;
+            carta.style.transition = "all 300ms linear,background-image 0ms ease-in-out"
+        }, 300)
+    }
+
+
+    #aciones() {
+        if (seleccionado) {
+            this.#selecTemporal = seleccionado;
+            seleccionado = false;
+            if (this.esIgual()) {
+                this.#eliminarEvento();
+                this.#selecTemporal.#eliminarEvento();
+            } else {
+                setTimeout(() => {
+                    this.#rotar()
+                    this.#selecTemporal.#rotar();
+                }, 1000)
+            }
+
+        } else {
+            seleccionado = this;
+        }
+    }
+
+
+
+
+    esIgual() {
+        return this.#num === this.#selecTemporal.#num && this.#carta !== this.#selecTemporal.#carta;
+    }
+
+
+
+
+
+}
+
+
+[1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6].sort(() => { return Math.random() > 0.5 ? 1 : -1 }).forEach((num, index) => {
+    if (index % 4 == 0 && index != 12) {
+        document.querySelector("table").append(document.createElement("tr"))
+    }
+    document.querySelector("table").lastChild.append((new Carta(num)).imprimir())
+
 })
+// document.querySelector("table").append((new Carta(1)).imprimir())
+// document.querySelector("table").append((new Carta(1)).imprimir())
+
+
+
