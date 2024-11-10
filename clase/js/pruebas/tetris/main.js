6fd74c597276b44afe058ae1159c72dd6be720e9
latifiyates <latifiyates@gmail.com>
2024-11-10 18:23:51 +0000

    TetrisFinDiseño

---
+++ clase/js/pruebas/tetris/main.js
@@ -0,0 +1,11 @@
+// document.querySelector("main").addEventListener("mousemove",(e)=>{
+//     let text = document.querySelector("#puntuaje")
+//     let ob = e.target.getBoundingClientRect()
+//     let x = e.clientX - ob.left
+//     let y = e.clientY - ob.top
+//     text.innerHTML = `x = ${x} y = ${y}`
+
+// })
+
+
+{[1,1,1][0,0,1]}
